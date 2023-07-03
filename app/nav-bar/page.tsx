22a7165d6cc1abe8d838b3b666e6893c3ac3835f
amoxley-dev <alex.moxley@gmail.com>
2023-07-03 13:46:57 -0700

    create basic button styling

--- app/nav-bar/page.tsx
+++ app/nav-bar/page.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link"
 
 export default function NavBar() { 
+    const navLinks: string[][] = [
+        ["About", "/about"],
+        ["Projects", "/projects"],
+        ["Resume", "/resume"]
+    ];
+
+    
     return (
-        <header>
-            <div>
-                <Link href="/">Alex Moxley</Link>
-            </div>
-            <ul>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link href="/resume">Resume</Link>
-                </li>
+        <header className="h-20 flex items-center justify-between border-2 border-red-600">
+            <h1 className="mx-6">
+                <Link href="/" className="text-slate-100">Alex Moxley</Link>
+            </h1>
+            <ul className="flex text-purple-100">
+                {navLinks.map(([name, path]) => (
+                    <li className="mx-6 text-center border-2 rounded-tl-2xl rounded-br-2xl border-violet-800 w-32 max-w-full bg-inherit bg-clip-padding">
+                        <Link className="max-h-full max-w-full p-2 block cursor-pointer" href={path}>{name}</Link>
+                    </li>
+                ))}
             </ul>
         </header>
     )
