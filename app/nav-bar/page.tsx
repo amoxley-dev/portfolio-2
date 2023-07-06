4d96394c370517c33c787399aa08ab6b2ed7f069
amoxley-dev <alex.moxley@gmail.com>
2023-07-05 21:31:23 -0700

    add text color

--- app/nav-bar/page.tsx
+++ app/nav-bar/page.tsx
@@ -9,11 +9,11 @@ export default function NavBar() {
 
     
     return (
-        <header className="h-20 flex items-center justify-between border-2 border-red-600">
+        <header className="h-20 flex items-center justify-between">
             <h1 className="mx-6">
                 <Link href="/" className="text-slate-100">Alex Moxley</Link>
             </h1>
-            <ul className="flex text-purple-100">
+            <ul className="flex text-purple-300">
                 {navLinks.map(([name, path]) => (
                     <li className="mx-6 text-center border-2 rounded-tl-2xl rounded-br-2xl border-violet-800 w-32 max-w-full bg-inherit bg-clip-padding">
                         <Link className="max-h-full max-w-full p-2 block cursor-pointer" href={path}>{name}</Link>
