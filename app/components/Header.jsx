ce688c0dd97dcd4a98a89a01a4d5316df8fd9368
amoxley-dev <alex.moxley@gmail.com>
2024-05-06 17:02:07 -0700

    create header components

---
+++ app/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Header = () => {
+  return (
+    <header>
+      Header
+      <NavBar />
+    </ header>
+  )
+}
+
+export default Header
