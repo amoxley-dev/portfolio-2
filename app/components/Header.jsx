2443579905aa43077d8d1193bfe6902e3d8c3d33
amoxley-dev <alex.moxley@gmail.com>
2024-05-07 17:31:00 -0700

    add NavBar links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavBar from './NavBar';
+import Logo from './Logo';
 
 const Header = () => {
   return (
     <header>
+      <Logo />
       Header
       <NavBar />
     </ header>
--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="/Resume">Resume</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
