98b882a413660fb9c4758c91a7dbf83e1d010c3e
amoxley-dev <alex.moxley@gmail.com>
2024-05-16 17:10:11 -0700

    create hamburger component

---
+++ app/components/Hamburger.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hamburger = () => {
+  return (
+    <>
+      <button id='mobile-open-button' className='text-3xl sm:hidden focus:outline-none'>
+        &#9776;
+      </button>
+    </>
+  )
+}
+
+export default Hamburger
