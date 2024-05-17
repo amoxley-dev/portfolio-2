bc6f11aa2550f152914346de633e67388d497796
amoxley-dev <alex.moxley@gmail.com>
2024-05-16 23:55:17 -0700

    update classes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const NavBar: React.FC = () => {
-  const defaultNavLink: string = 'hover:opacity-85';
-  const activeNavLink: string = 'text-turquoise-blue-50'
+  const defaultNavLink: string = 'link-underline';
+  const activeNavLink: string = 'text-turquoise-blue-50 active-link-underline'
   const pathname: string = usePathname();
 
   return (
     <>
-      <nav className='hidden sm:block space-x-8 text-xl z-20' aria-label='main'>
+      <nav className='hidden sm:block space-x-8 text-x' aria-label='main'>
         <Link href='/' className={pathname === '/' ? activeNavLink : defaultNavLink}>Home</Link>
         <Link href='/projects' className={pathname === '/projects' ? activeNavLink : defaultNavLink}>Projects</Link>
         <Link href='/resume'className={pathname === '/resume' ? activeNavLink : defaultNavLink}>Resume</Link>
