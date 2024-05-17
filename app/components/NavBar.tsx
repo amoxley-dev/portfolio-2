1d14678f679b2011d3815c66924336fa55bb1caa
amoxley-dev <alex.moxley@gmail.com>
2024-05-16 21:50:49 -0700

    add active link markers

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,19 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const NavBar: React.FC = () => {
-  const navLinkClass: string = 'hover:opacity-85 focus:text-turquoise-blue-50 focus:font-bold'  
+  const defaultNavLink: string = 'hover:opacity-85';
+  const activeNavLink: string = 'text-turquoise-blue-50'
+  const pathname: string = usePathname();
 
   return (
     <>
       <nav className='hidden sm:block space-x-8 text-xl ' aria-label='main'>
-        <Link href='/' className={navLinkClass}>Home</Link>
-        <Link href='/projects' className={navLinkClass}>Projects</Link>
-        <Link href='/Resume'className={navLinkClass}>Resume</Link>
+        <Link href='/' className={`${defaultNavLink} ${pathname === '/' ? activeNavLink : ''}`}>Home</Link>
+        <Link href='/projects' className={`${defaultNavLink} ${pathname === '/projects' ? activeNavLink : ''}`}>Projects</Link>
+        <Link href='/resume'className={`${defaultNavLink} ${pathname === '/resume' ? activeNavLink : ''}`}>Resume</Link>
       </nav>
     </>
   )
