7020df7650146c9a82f30a8c8e7087543219fad6
amoxley-dev <alex.moxley@gmail.com>
2024-05-30 15:56:35 -0700

    create MobileNavBar component

---
+++ app/components/MobileNavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MobileNavBar = () => {
+  return (
+    <>
+      <button className='text-8xl self-end px-6'>
+        &times;
+      </button>
+    </>
+  )
+}
+
+export default MobileNavBar
