847b156f75ff7ddedb9e6d1c3004d0ee2bab579d
amoxley-dev <alex.moxley@gmail.com>
2024-05-31 15:52:55 -0700

    add toggle menu function for hamburger menu

--- app/components/Hamburger.tsx
+++ app/components/Hamburger.tsx
@@ -1,9 +1,15 @@
+'use client'
 import React from 'react'
+import toggleMenu from '../utils/toggleMenu'
 
 const Hamburger = () => {
   return (
     <>
-      <button id='mobile-open-button' className='text-3xl sm:hidden focus:outline-none'>
+      <button 
+        id='mobile-open-button' 
+        className='text-3xl sm:hidden focus:outline-none'
+        onClick={() => toggleMenu()}
+      >
         &#9776;
       </button>
     </>
