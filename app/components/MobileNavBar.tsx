f4332ca613db2cc352286afc3a4daf4e594ac76f
amoxley-dev <alex.moxley@gmail.com>
2024-05-31 15:53:40 -0700

    add toggleMenu and update component layout

--- app/components/MobileNavBar.tsx
+++ app/components/MobileNavBar.tsx
@@ -1,11 +1,31 @@
+'use client'
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import toggleMenu from '../utils/toggleMenu'
 
 const MobileNavBar = () => {
+  const pathname: string = usePathname();
+  const activeNavLink: string = 'mobile-link-underline';
+
   return (
     <>
-      <button className='text-8xl self-end px-6'>
-        &times;
-      </button>
+      <section 
+        id='mobile-menu' 
+        className='absolute bg-black top-0 w-full text-5xl flex-col justify-center origin-top animate-open-menu hidden'
+        onClick={() => toggleMenu()}
+      >
+        <button 
+          className='text-8xl self-end px-6'
+        >
+          &times;
+        </button>
+        <nav className='flex flex-col min-h-screen items-center py-8'>
+          <Link href='/' className={pathname === '/' ? activeNavLink : ''}>Home</Link>
+          <Link href='/projects' className={pathname === '/projects' ? activeNavLink : ''}>Projects</Link>
+          <Link href='/resume'className={pathname === '/resume' ? activeNavLink : ''}>Resume</Link>
+        </nav>
+      </section>
     </>
   )
 }
