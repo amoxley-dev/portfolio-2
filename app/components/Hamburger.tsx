c654652837baadee76ab22c9bbabf8f17c56b627
amoxley-dev <alex.moxley@gmail.com>
2024-06-03 16:20:16 -0700

    create html element hamburger icon

--- app/components/Hamburger.tsx
+++ app/components/Hamburger.tsx
@@ -6,11 +6,11 @@ const Hamburger = () => {
   return (
     <>
       <button 
-        id='mobile-open-button' 
-        className='text-3xl sm:hidden focus:outline-none'
+        id='hamburger-button' 
+        className='text-3xl sm:hidden relative w-8 h-8'
         onClick={() => toggleMenu()}
       >
-        &#9776;
+        <div className="absolute top-4 -mt-0.5 h-1 w-8 rounded bg-white transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-white after:transition-all after:duration-500 after:content-['']"></div>
       </button>
     </>
   )
