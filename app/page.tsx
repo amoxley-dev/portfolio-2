26cc8258ef360f4a624ea709b82e7d4328bfb85e
amoxley-dev <alex.moxley@gmail.com>
2024-06-21 16:41:15 -0700

    add home content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      Home Page
+      <section className="flex flex-col justify-center text-2xl my-3">
+        <div>
+          Welcome to my portfolio! I am a software engineer with full stack experience building scalable, accessible, and dynamic websites.
+        </div>
+        <div className="[&>a]:text-turquoise-blue-800">
+          Feel free to check out my <Link href='/projects'>projects</Link> or my <Link href='/resume'>resume</Link>.
+        </div>
+        <div className="[&>a]:text-turquoise-blue-800">
+          If you want to get in contact with me, feel free to send an email to <a href="mailto:alex.moxley@gmail.com">alex.moxley@gmail.com</a>
+        </div>
+      </section>
     </main>
   );
 }
