213136d6743956c7d34ec64af1f3f48cc65e72c2
amoxley-dev <alex.moxley@gmail.com>
2024-06-21 15:26:14 -0700

    create loading spinner component

---
+++ app/components/LoadingSpinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Spinner } from '@nextui-org/spinner'
+
+const LoadingSpinner = () => {
+  return (
+    <>
+      <Spinner size='lg' color='primary' className='flex h-96'/>
+    </>
+  )
+}
+
+export default LoadingSpinner
