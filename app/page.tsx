235468d126f3145c27160e440dadb9ff0048414b
amoxley-dev <alex.moxley@gmail.com>
2024-06-26 16:06:55 -0700

    add Grid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Grid from "./components/Grid";
 
 export default function Home() {
   return (
-    <main className="flex justify-center">
-      <section className="flex flex-col justify-center text-2xl my-3">
+    <main className="flex justify-center flex-col items-center">
+      <section className="flex justify-center flex-col items-center text-2xl my-3">
         <div>
-          Welcome to my portfolio! I am a software engineer with full stack experience building scalable, accessible, and dynamic websites.
+          Welcome to my portfolio!
+        </div>
+        <div>
+          I am a software engineer with full stack experience building scalable, accessible, and dynamic websites.
         </div>
         <div className="[&>a]:text-turquoise-blue-800">
           Feel free to check out my <Link href='/projects'>projects</Link> or my <Link href='/resume'>resume</Link>.
@@ -14,6 +18,7 @@ export default function Home() {
           If you want to get in contact with me, feel free to send an email to <a href="mailto:alex.moxley@gmail.com">alex.moxley@gmail.com</a>
         </div>
       </section>
+      <Grid />
     </main>
   );
 }
